c3fb60890f4b70d62702e9e13688fff069f115b8
dougcarvalho <douglas.descarvalho@gmail.com>
2023-01-10 20:56:35 -0300

    Lacos de repeticao
    Aprendizado de lacos

--- Douglas/main.py
+++ Douglas/main.py
@@ -1,1 +1,30 @@
-print('Hello World')
+import random
+
+#print('Hello World')
+
+#baseado em uma condicional
+num = random.randint(1,1000)
+contador = 0
+
+#baseado em condicionais
+while num != 2:
+    num = random.randint(1,1000)
+    contador += 1
+
+print('vezes que jogamos o dado:', contador)
+
+
+
+
+#baseado em numero de repeticoes
+i = 0
+for i in range(5):
+    num1 = random.randint(1,100)
+    print (num1)
+print ('vezes que jogamos o dado:', i)
+
+
+
+
+
+
